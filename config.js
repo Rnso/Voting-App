f591dc6b985732767ecf475dfbc0f1b69dc23846
Rnso <rnso.pnr@gmail.com>
2017-09-11 14:53:32 +0400

    commit github with final changes

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const env = process.env
 export const nodeEnv = env.NODE_ENV || 'development'
 
 export default {
-    mongodbUri: `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@ds159892.mlab.com:59892/fcc`,
+    mongodbUri: 'mongodb://localhost:27017/bookClub',
     port: env.PORT || 3000,
     host: env.HOST || '0.0.0.0'
 }
--- constant.js
+++ constant.js
@@ -1,5 +1,5 @@
-export const serverUrl = 'https://voting-ground.herokuapp.com'
 
+export const serverUrl = 'http://localhost:3000'
 
 let session  = typeof sessionStorage['obj'] !=  'undefined' ? JSON.parse(sessionStorage.obj) : ''
 export const USERID = session._id
