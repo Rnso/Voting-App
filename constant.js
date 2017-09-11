3762d97781d32f233a27393ec809592144f68da9
Rnso <rnso.pnr@gmail.com>
2017-09-11 14:49:27 +0400

    commit

--- constant.js
+++ constant.js
@@ -1,6 +1,5 @@
 export const serverUrl = 'https://voting-ground.herokuapp.com'
 
 
-
 let session  = typeof sessionStorage['obj'] !=  'undefined' ? JSON.parse(sessionStorage.obj) : ''
 export const USERID = session._id
