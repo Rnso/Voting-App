737c131e05be4222a2f2c8a42162f1f0d23c7303
Rnso <rnso.pnr@gmail.com>
2017-09-11 14:25:31 +0400

    config changes

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const env = process.env
 
 export const nodeEnv = env.NODE_ENV || 'development'
 
-export default{
+export default {
     mongodbUri: `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@ds159892.mlab.com:59892/fcc`,
     port: env.PORT || 3000,
     host: env.HOST || '0.0.0.0'
